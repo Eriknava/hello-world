4db2c6139fab98465e56d8b7515e5cecd64ec2f0
Erik Navasardjan <erik.navasardjan@gmail.com>
2020-03-18 21:11:16 +0100

    Uppdatering
    Fixade index.php samt styles.css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>Hello Erik</h1>
+<p>Krisse lär Erik allt han kan</p>
+
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+
+</body>
+</html>
